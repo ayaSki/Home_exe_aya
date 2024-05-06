d93dd484736a50510002f2c5431ca5b5e7c42769
Aya Sobolevski <aya.ski12@gmail.com>
2024-05-07 01:41:47 +0300

    Adde the function pythagorean_triplet

--- pythagorean_triplet_by_sum.py
+++ pythagorean_triplet_by_sum.py
@@ -2,26 +2,23 @@ import math
 
 
 def get_pythagorean_triplet_by_even(even: int) -> list:
-    return [math.pow(even / 2, 2) - 1, math.pow(even / 2, 2) - 1]
+    return [int((even/2)**2 - 1), int((even/2)**2 + 1)]
 
 
 def get_pythagorean_triplet_by_odd(odd: int) -> list:
-    return [math.pow(odd, 2) / 2 - 0.5, math.pow(odd, 2) / 2 - 0.5]
+    return [int((odd**2)/2 - 0.5), int((odd**2)/2 + 0.5)]
 
 
 def pythagorean_triplet_by_sum(sum: int) -> None:
     first_pythagorean_num = 1
     options = []
-    found = False
-    while not found:
+    while first_pythagorean_num < sum:
         if first_pythagorean_num % 2 == 0:
             options = get_pythagorean_triplet_by_even(first_pythagorean_num)
-        options = get_pythagorean_triplet_by_odd(first_pythagorean_num)
+        else:
+            options = get_pythagorean_triplet_by_odd(first_pythagorean_num)
         if first_pythagorean_num + options[0] + options[1] == sum:
             print(first_pythagorean_num, options[0], options[1])
-            found = True
+            return
         first_pythagorean_num += 1
-
-
-if __name__ == '__main__':
-    pythagorean_triplet_by_sum(17)
+    print("There is no such a pythagorean triplet")

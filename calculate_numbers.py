d9ad316167c95ac172bba5ee5717e8d7d47097fa
Aya Sobolevski <aya.ski12@gmail.com>
2024-05-10 14:44:16 +0300

    Added 2.4 without bonus

--- calculate_numbers.py
+++ calculate_numbers.py
@@ -1,10 +1,13 @@
+from typing import List, Any
+
+
 def get_numbers() -> list:
     print("Enter numbers, when you want to stop enter -1\n")
     number = 0
     numbers = []
     while number != "-1":
         number = input("Enter number: ")
-        if number.isdigit() and number != "-1":
+        if number != "-1":
             numbers.append(int(number))
     return numbers
 
@@ -13,19 +16,21 @@ def get_average(numbers: list) -> int:
     return sum(numbers)//len(numbers)
 
 
-def get_positive_numbers(numbers: list) -> int:
-    return len([i for i in range(len(numbers)) if i > 0])
+def get_positive_numbers(numbers: list) -> list:
+    return [number for number in numbers if number > 0]
 
 
-def return_sorted_list(numbers: list) -> list:
-    return numbers.sort()
+def return_sorted_list(numbers: list) -> None:
+    numbers.sort()
 
 
 def exe_flow() -> None:
     numbers = get_numbers()
     print(f"Your numbers average: {get_average(numbers)}\n"
-          f"All the positive numbers: {get_positive_numbers(numbers)}\n"
-          f"Sorted numbers: {return_sorted_list(numbers)}")
+          f"All the positive numbers: {get_positive_numbers(numbers)}")
+    return_sorted_list(numbers)
+    print(f"Sorted numbers: {numbers}")
+
 
 if __name__ == '__main__':
     exe_flow()
